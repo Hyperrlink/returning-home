32db1f5de5b9b53c15db43286b7514481545284f
HyperrLink <42501030+Hyperrlink@users.noreply.github.com>
2019-12-31 17:37:09 +0200

    Getting ready for the demo

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject hostItems;
+    public GameObject clientItems;
+
+    void Start()
+    {
+
+        hostItems.SetActive(false);
+        clientItems.SetActive(false);
+
+    }
+
+    public void StartGame()
+    {
+
+        SceneManager.LoadScene("Hub");
+
+    }
+
+    public void Exit()
+    {
+
+        Application.Quit();
+
+    }
+
+}
